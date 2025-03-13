b356015bf4ac6c73a8693db6e0181aad9709e1fb
HP <zeinabmostafa11111@gmail.com>
2025-03-13 02:43:03 +0200

    make changes in bottomSheet

--- lib/view/notes_view.dart
+++ lib/view/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/view/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/view/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -8,7 +9,15 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)),
+              context: context,
+              builder: (context) {
+                return AddNoteBottomSheet();
+              });
+        },
         child: const Icon(Icons.add),
         backgroundColor: Colors.greenAccent.shade100,
       ),
