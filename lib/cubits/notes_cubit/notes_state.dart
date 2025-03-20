922f832fb96f2c5559d609dc91ea85990748fe06
HP <zeinabmostafa11111@gmail.com>
2025-03-20 23:09:58 +0200

    refresh notes list

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -6,9 +6,3 @@ sealed class NotesState {}
 final class NotesInitial extends NotesState {}
 
 final class NotesSuccess extends NotesState {}
-
-// final class NoteFailure extends NotesState {
-//   final String errorMessage;
-
-//   NoteFailure(this.errorMessage);
-// }
